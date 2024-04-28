893906392900ab3bb749650622ff0ab3686e297d
HoHong123 <epzmfhs@gmail.com>
2024-04-29 07:59:43 +0900

    :new: feat : Add controller/module/service of login, boards, auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BoardsModule } from './boards/boards.module';
+import { LoginModule } from './login/login.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [BoardsModule, LoginModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
