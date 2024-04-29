2aee45569b937e64ac020ae27f385161d9c722d4
HoHong123 <epzmfhs@gmail.com>
2024-04-29 10:57:47 +0900

    :new: feat : Add first service function
    1. 첫 서비스 테스트
    + 컨트롤러와 서비스를 연결
    ++ '도메인/boards' 주소로 전환
    ++ 주소 입력시 컨트롤러에서 요청을 서비스가 핸들링하여 요청을 컨트롤러로 반환하여 결과를 출력한다.

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { BoardsService } from './boards.service';
 
 @Controller('boards')
-export class BoardsController {}
+export class BoardsController {
+    constructor(private boardService: BoardsService) {}
+
+    // GET, POST, DELETE, PUT
+    @Get() // 도메인/boards
+    getAllBoard() {
+        return this.boardService.getAllBoards();
+    }
+}
