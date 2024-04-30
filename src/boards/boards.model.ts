e44418d79bec10f034ddaf9e9a74cf561c2c8f1b
HoHong123 <epzmfhs@gmail.com>
2024-04-30 11:41:38 +0900

    :new: feat : Add board models
    1. Adding Board interface
    2. Adding Board enum

---
+++ src/boards/boards.model.ts
@@ -0,0 +1,14 @@
+// INTERFACE
+export interface Board {
+    id: string;
+    title: string;
+    description: string;
+    status : BoardStatus;
+}
+
+
+// ENUM
+export enum BoardStatus{
+    PUBLIC = 'PUBLIC',
+    PRIVATE = 'PRIVATE',
+}
