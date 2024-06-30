98950418521fa623c568f3c501e41d57ef00a1ee
JasonCatze <jason@catze.xyz>
2024-07-01 01:28:01 +0900

    :new: feat : Add basic interceptor

---
+++ src/request-interceptor/request-interceptor.interceptor.spec.ts
@@ -0,0 +1,7 @@
+import { RequestInterceptorInterceptor } from './request-interceptor.interceptor';
+
+describe('RequestInterceptorInterceptor', () => {
+  it('should be defined', () => {
+    expect(new RequestInterceptorInterceptor()).toBeDefined();
+  });
+});
