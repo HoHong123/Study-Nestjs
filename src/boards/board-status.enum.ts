ddc33148d4035b44357889aabcf420142a8e2397
JasonCatze <jason@catze.xyz>
2024-07-01 22:33:14 +0900

    :recycle: refactor : Edit enum
    1. 문자열을 정수형으로 변경

--- src/boards/board-status.enum.ts
+++ src/boards/board-status.enum.ts
@@ -1,6 +1,9 @@
 /** @format */
 
 export enum BoardStatus {
-	PUBLIC = 'PUBLIC',
-	PRIVATE = 'PRIVATE',
+	PUBLIC 	= 1,
+	PRIVATE = 2,
 }
+
+// enum을 string 혹은 int로 선언할 수 있으나, int형이 기술적으로 이점이 많다.
+// + 확장성을 고려하여, 쿼리문 요청이나 데이터베이스 보관에도 더욱 용이하다.
